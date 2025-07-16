e58ac8791b142b5b25c5d371a043a9032315cebf
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2025-07-16 20:21:44 -0300

    Update ServiceList.tsx

--- frontend/src/components/ServiceList.tsx
+++ frontend/src/components/ServiceList.tsx
@@ -18,9 +18,9 @@ export default function ServiceList({
     return (
         <div className="flex items-center justify-center flex-wrap gap-[25px]">
             {loadedServices?.length > 0 ? loadedServices.map((service) => (
-                <div key={service.id} className="border p-2">
+                <div key={service.id} className="border p-2 rounded-[10px]">
                     <p> {service.description} </p>
-                    <p> R$ {service.price.toFixed(2).replace('.', ',')} {typeof service.price} </p>
+                    <p> R$ {service.price.toFixed(2).replace('.', ',')} </p>
                 </div>
             )) : (
                 <p className="opacity-50"><i> Nenhum serviço encontrado </i></p>
