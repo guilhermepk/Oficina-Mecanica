377e4bd649b4df858cc4f0478968bf48ffa490a0
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2025-07-16 08:13:24 -0300

    ajustes iniciais

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASS'),
+        database: config.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,52 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import * as cors from "cors";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger("Gerenciador de projetos");
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }));
+
+  let currentRoute = null;
+  let currentRequest = null;
+  app.use((req, _, next) => {
+    currentRoute = req.path;
+    currentRequest = req;
+    next();
+  });
+
+  app.use(cors({
+    origin: function (origin, callback) {
+      const whiteList = process.env.CORS_WHITELIST ? process.env.CORS_WHITELIST.split(', ') : [];
+      if (whiteList.includes(origin) || !origin) {
+        if (process.env.PRODUCTION === 'false') logger.debug(`Origem permitida através do CORS. Origem ${origin} na rota ${currentRoute}`);
+
+        callback(null, true)
+      } else {
+        logger.error(`Origem não permitida através do CORS. Origem ${origin} na rota ${currentRoute}`);
+        console.log('request', currentRequest)
+      }
+    },
+    methods: [
+      "GET",
+      "POST",
+      "PATCH",
+      "DELETE"
+    ],
+    credentials: true,
+  }));
+
+  const port = process.env.PORT ?? 3000;
+
+  await app.listen(port);
+
+  logger.log(`Backend de ${process.env.PRODUCTION === 'true' ? 'produção' : 'desenvolvimento'} rodando na porta ${port}`);
 }
 bootstrap();
